9f85a6935e6f490563983f7a612318fe20cb6508
yzn7020@163.com <yzn7020@163.com>
2019-08-26 17:25:38 +0800

    8.26

---
+++ day19/s14day19_2/app01/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, HttpResponse, redirect
+from app01 import models
+
+
+# Create your views here.
+def login(request):
+    error_msg = ''
+    if request.method == 'POST':
+        username = request.POST.get("username")
+        psw = request.POST.get('password')
+        obj = models.UserInfo.objects.filter(username=username, password=psw)
+        print(obj)
+        if obj:
+            # return render(request, 'index.html')
+            return redirect('/cmdb/index/')
+        else:
+            error_msg = "用户名或密码错误！"
+    return render(request, 'login.html', {'error_msg': error_msg})
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def orm(request):
+    models.UserInfo.objects.create(username='root', password='123')
+    return HttpResponse("orm")
