15a77571d6011d507af8f2fd8a2b9a2928094256
yzn7020@163.com <yzn7020@163.com>
2019-09-09 17:42:33 +0800

    9.9

--- day20/day20_1/app01/views.py
+++ day20/day20_1/app01/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from app01 import models
 # Create your views here.
 
@@ -9,11 +9,22 @@ def business(request):
 
 
 def host(request):
-    v1 = models.HOST.objects.all()
-    v2 = models.HOST.objects.filter(nid__gt=0).values('hostname', 'ip', 'port', 'b__caption')
-    # for row in v2:
-    #     print(row['b__caption'])
-    v3 = models.HOST.objects.filter(nid__gt=0).values_list('hostname', 'ip', 'port', 'b__caption')
-    # for row in v3:
-    #     print(row[3])
-    return render(request, 'host.html', {'v1': v1, 'v2': v2, 'v3': v3})
+    if request.method == "GET":
+        v1 = models.HOST.objects.all()
+        v2 = models.HOST.objects.filter(nid__gt=0).values(
+            'hostname', 'ip', 'port', 'b__caption')
+        # for row in v2:
+        #     print(row['b__caption'])
+        v3 = models.HOST.objects.filter(nid__gt=0).values_list(
+            'hostname', 'ip', 'port', 'b__caption')
+        # for row in v3:
+        #     print(row[3])
+        b_list = models.Business.objects.all()
+        return render(request, 'host.html', {'v1': v1, 'v2': v2, 'v3': v3, 'b_list': b_list})
+    elif request.method == "POST":
+        h = request.POST.get('hostname')
+        i = request.POST.get('ip')
+        p = request.POST.get('port')
+        b = request.POST.get('b_id')
+        models.HOST.objects.create(hostname=h, ip=i, port=p, b_id=b)
+        return redirect('/cmdb/host/')
--- day20/day20_1/day20_1/settings.py
+++ day20/day20_1/day20_1/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'day20_1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR, os.path.join('templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,6 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIR = (
-    [BASE_DIR, os.path.join('static')],
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
 )
